984ae00c3cb30bd9e42a3330bae5ffca84497c26
zhangwuji0003 <work0003@qq.com>
2020-03-05 12:13:04 +0800

    first commit

---
+++ scripts/test_jk1.py
@@ -0,0 +1,12 @@
+class TestJk1:
+    def test_case1(self):
+        assert 1
+
+    def test_case2(self):
+        assert 1
+
+    def test_case3(self):
+        assert 0
+
+    def test_case4(self):
+        assert 1
